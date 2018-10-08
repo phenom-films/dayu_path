a353debf6bc1a2142f74853ca690487f6185daf4
Andy Guo <andyguo@phenom-films.com>
2018-10-08 11:28:49 +0800

    init

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+        name='dayu_path',
+        version='0.2',
+        packages=['test', 'dayu_path'],
+        url='https://github.com/phenom-films/dayu_path',
+        license='MIT',
+        author='andyguo',
+        author_email='andyguo@phenom-films.com',
+        description='a python path lib optimized for Movie industry. Support scan for sequence files, extract frame count and many other useful functions.',
+        long_description = open('README.rst').read(),
+        classifiers=[
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: Implementation',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+        ],
+)
