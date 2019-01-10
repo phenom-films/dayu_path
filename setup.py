96e39c5268949cd51013777b22f974466744ead5
muyanru <muyanru@phenom-films.com>
2019-01-10 16:49:52 +0800

    change version from 0.3 -> 0.31

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='dayu_path',
-        version='0.3',
+        version='0.3.1',
         packages=['test', 'dayu_path'],
         url='https://github.com/phenom-films/dayu_path',
         license='MIT',
